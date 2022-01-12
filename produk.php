b2b8acde9eebf4faf9e216d5956c420788a14830
SilviaR318 <97298169+SilviaR318@users.noreply.github.com>
2022-01-12 23:04:42 +0700

    Update produk.php

--- produk.php
+++ produk.php
@@ -1,3 +1,10 @@
+<?php
+session_start();
+if (empty($_SESSION["username"])) {
+    header('Location: http://localhost/penjualan-buku/login.php');
+    exit;
+}
+?>
 <html>
     <head>
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
