ba8d6fdc67ee1a90783a4a2ba1bdf8cf09311ee2
Silvia Rosita <97298169+SilviaR318@users.noreply.github.com>
2022-03-15 11:16:14 +0700

    Update edit.php

--- edit.php
+++ edit.php
@@ -5,7 +5,10 @@ if (!isset($_SESSION["username"])) {
   header("Location : login.php");
   exit;
 }
-
+if ($_SESSION["role_id"] != 1) {
+    header('Location: http://localhost/penjualan-buku/login.php');
+    exit;
+}
 include "koneksi.php";
  if ($_GET['hal']=="edit") {
      
