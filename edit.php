55c09d06848bfcc46c7d1d5426f9c35ceac735e3
Galang Aidil Akbar <73085251+Galangaidil@users.noreply.github.com>
2022-03-17 10:43:16 +0700

    Using Reusable Components (Sidebar)

--- edit.php
+++ edit.php
@@ -1,210 +1,157 @@
 <?php
-
 session_start();
-if (!isset($_SESSION["username"])) {
-  header("Location : login.php");
-  exit;
+
+if (empty($_SESSION["username"])) {
+    header('Location: http://localhost/penjualan-buku/login.php');
+    exit;
 }
 if ($_SESSION["role_id"] != 1) {
     header('Location: http://localhost/penjualan-buku/index.php');
     exit;
 }
-include "koneksi.php";
- if ($_GET['hal']=="edit") {
-     
-    $sql = "SELECT * FROM buku WHERE kd_buku ='$_GET[kd_buku]'";
-    $query= mysqli_query($conn, $sql);
-    $prd= mysqli_fetch_array($query);
-    if ($prd) {
-       $Foto=$prd['foto'];
-       $Judul=$prd['judul'];
-       $Deskripsi=$prd['deskripsi'];
-       $Pengarang=$prd['pengarang'];
-       $Penerbit=$prd['penerbit'];
-       $stok=$prd['stok'];
-       $Harga=$prd['harga'];
-       
-    }
- }
 
- if(isset($_POST['submit'])){
- 
- 	
+$username = $_SESSION["username"];
+
+require_once('components.php');
+
+$components = new Components();
+
+include "koneksi.php";
+if ($_GET['hal'] == "edit") {
+
+    $sql = "SELECT * FROM buku WHERE kd_buku ='$_GET[kd_buku]'";
+    $query = mysqli_query($conn, $sql);
+    $prd = mysqli_fetch_array($query);
+    if ($prd) {
+        $Foto = $prd['foto'];
+        $Judul = $prd['judul'];
+        $Deskripsi = $prd['deskripsi'];
+        $Pengarang = $prd['pengarang'];
+        $Penerbit = $prd['penerbit'];
+        $stok = $prd['stok'];
+        $Harga = $prd['harga'];
+    }
+}
+
+if (isset($_POST['submit'])) {
+
+
     $Foto = $_POST['Foto'];
-    $Judul= $_POST['Judul'];
-    $Deskripsi= $_POST['Deskripsi'];
-    $Pengarang= $_POST['Pengarang'];
+    $Judul = $_POST['Judul'];
+    $Deskripsi = $_POST['Deskripsi'];
+    $Pengarang = $_POST['Pengarang'];
     $Penerbit = $_POST['Penerbit'];
     $stok = $_POST['stok'];
     $Harga = $_POST['Harga'];
-   
-    
+
+    var_dump($Judul);
+
+
     $sql = "UPDATE buku SET foto='$Foto',
    judul='$Judul', deskripsi='$Deskripsi', pengarang='$Pengarang',
    penerbit='$Penerbit', stok='$stok', harga='$Harga' WHERE kd_buku='$_GET[kd_buku]'";
     $query = mysqli_query($conn, $sql);
-    
-        if( $query ) {
-   
+
+    if ($query) {
+
         // kalau berhasil alihkan ke halaman admin.php
         echo "<script>
              alert('data berhasil diedit!');
              document.location.href = 'databuku.php';
            </script>";
-        } else {
+    } else {
         // kalau gagal tampilkan pesan
+        echo "<script>
+             alert('data gagal diedit!');
+           </script>";
         die("Gagal menyimpan perubahan...");
-        }
-   
-    } 
-    
-   
-   
+    }
+}
+
 ?>
 
+<!doctype html>
+<html lang="en">
 
-
-<html>
-    <head>
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
     <link rel="icon" type="image/png" href="images/home.png">
-        <title>Admin | SibulainStore</title>
-        <link rel="stylesheet" href="css/index.css">
-        <link rel="preconnect" href="https://fonts.gstatic.com">
-<link href="https://fonts.googleapis.com/css2?family=Poppins:wght@200;300;600&display=swap" rel="stylesheet">
-<script src="https://kit.fontawesome.com/a076d05399.js"></script>
-        <style type="text/css">
 
-        *{
-            margin: 0;
-            padding: 0;
-            font-family: 'Times New Roman', Times, serif;
-        }
+    <title>Admin | SibulainStore</title>
+</head>
 
-        
+<body>
 
-        .login{
-            width: 500px;
-            height: 600px;
-            background: radial-gradient( #008B8B, #fff);
-            backdrop-filter: blur(1000vh);
-            box-sizing: border-box;
-            border-radius: 20px;
-            position: absolute;
-            top: 30%;
-            left:500px;
-            margin: auto;
-            color:  #555;
-            padding: 70px 40px;
-            text-align: center;
-            
-        }
+    <?= $components->sidebar($username) ?>
 
-        .avatar{
-            border-radius: 50%;
-            position: absolute;
-            top: -70px;
-            left: 35%;
-        }
+    <div>
+        <div class="row py-3">
+            <div class="col-10 col-md-8 mx-auto">
+                <div class="col-sm">
+                    <h2>Edit buku <?= $Judul ?></h2>
+                </div>
+            </div>
+        </div>
+    </div>
 
-        h1{
-           text-align: center;
-            font-size: 25px;
-            padding: 20px;
+    <div>
+        <div class="row py-2">
+            <div class="col-10 col-md-5 mx-auto pb-5">
 
-            
-           
-        }
+                <form action="" method="POST">
 
-        .login p{
-            margin: 0;
-            padding: 0;
-            font-weight: bold;
+                    <div class="mb-3">
+                        <label for="gambar" class="form-label">Foto</label>
+                        <input class="form-control" type="file" name="Foto" id="gambar" required>
+                    </div>
 
-        }
+                    <div class="mb-3">
+                        <label for="judul" class="form-label">Judul</label>
+                        <input type="text" class="form-control" id="judul" name="Judul" placeholder="Judul buku" value="<?= $Judul ?>" required>
+                    </div>
 
-        .login input{
-            width: 100%;
-            margin-bottom: 20px;
-        }
+                    <div class="mb-3">
+                        <label for="deskripsi" class="form-label">Deskripsi</label>
+                        <textarea class="form-control" id="deskripsi" rows="3" name="Deskripsi"><?= $Deskripsi ?></textarea>
+                    </div>
 
-        .login input[type="text"]
-        {
-            border: none;
-            border-bottom: 1px solid #fff;
-            background: transparent;
-            color: black;
-            font-size: 16px;
+                    <div class="mb-3">
+                        <label for="pengarang" class="form-label">Pengarang</label>
+                        <input type="text" class="form-control" id="pengarang" name="Pengarang" placeholder="J. K. Rowling" value="<?= $Pengarang ?>" required>
+                    </div>
 
-           
-        }
+                    <div class="mb-3">
+                        <label for="penerbit" class="form-label">Penerbit</label>
+                        <input type="text" class="form-control" id="penerbit" name="Penerbit" placeholder="Gramedia" value="<?= $Penerbit ?>" required>
+                    </div>
+
+                    <div class="mb-3">
+                        <label for="stok" class="form-label">Stok</label>
+                        <input type="number" class="form-control" id="stok" name="stok" placeholder="1000" value="<?= $stok ?>" required>
+                    </div>
+
+                    <div class="mb-3">
+                        <label for="harga" class="form-label">Harga</label>
+                        <input type="number" class="form-control" id="harga" name="Harga" placeholder="RP.54,000" value="<?= $Harga ?>" required>
+                    </div>
 
 
-        .tombol{
-            width: 100%;
-            padding: 8px 10px;
-            outline: none;
-            border: 2px solid#fff;
-            border-radius: 20px;
-            background: transparent;
-            color: black;
-            font-size: 12px;
-            float: right;
-            text-align: center;
-            cursor: pointer;
-        }
+                    <div class="d-flex justify-content-between">
+                        <button class="btn btn-outline-secondary" type="reset">Batal</button>
+                        <button class="btn btn-primary" type="submit" name="submit">Simpan</button>
+                    </div>
 
-    
-        </style>
-    </head>
-    <body>
-        
-    <div class="header">
-       
-       <div class="container">
-           <div class="navbar">
-               
-               <nav>
-                   <ul id="menuitems">
-                       <li><a href="home.html">Home</a></li>
-                       <li><a href="">About Us</a></li>
-                       <li><a href="produc.html">All Product</a></li>
-                       
-                   </ul>
-               </nav>
-               <img src="images/cart.png" width="30px" height="30px">
-               <img src="images/menu.png" class="menu-icn" width="30px" height="30px">
-       </div>
-       
-       </div>
-       </div>
-
-        
-        <div class="login">
-             <img src="images/home.png" height="150" width="150" class="avatar">
-                <h1>DATA BUKU</h1>
-                
-                <form action="" method="POST" acttion="" >
-
-                    <label>Foto</label>
-                    <input type="file" name="Foto" value="<?php echo $Foto?>"> 
-                    <label>Judul Buku</label>
-                    <input type="text" name="Judul" value="<?php echo $Judul?>" >
-                    <label>Deskripsi Buku</label>
-                    <input type="text" name="Deskripsi" value="<?php echo $Deskripsi?>">
-                    <label>Pengarang</label>
-                    <input type="text" name="Pengarang"  value="<?php echo $Pengarang?>">
-                    <label>Peneribit</label>
-                    <input type="text" name="Penerbit" value="<?php echo $Penerbit?>">
-                    <label>stok</label>
-                    <input type="text" name="stok" value="<?php echo $stok?>">
-                    <label>Harga</label>
-                    <input type="text" name="Harga" value="<?php echo $Harga?>">
-                    <input type="submit" name="submit"  class="tombol"  >  
-                    
                 </form>
 
+            </div>
+        </div>
+    </div>
 
+    </main>
 
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+</body>
 
-    </body>
 </html>
--- input.php
+++ input.php
@@ -10,6 +10,10 @@ if ($_SESSION["role_id"] != 1) {
 }
 
 $username = $_SESSION["username"];
+
+require_once('components.php');
+
+$components = new Components();
 ?>
 
 <!doctype html>
@@ -25,116 +29,73 @@ $username = $_SESSION["username"];
 </head>
 
 <body>
-    <div class="offcanvas offcanvas-start" tabindex="-1" id="sidebar" aria-labelledby="sidebarLabel">
-        <div class="offcanvas-header">
-            <h5 class="offcanvas-title" id="sidebarLabel">Sibulain</h5>
-            <button type="button" class="btn-close text-reset" data-bs-dismiss="offcanvas" aria-label="Close"></button>
-        </div>
-        <div class="offcanvas-body">
-            <ul class="nav flex-column">
-                <li class="nav-item">
-                    <a class="nav-link" href="databuku.php">Dashboard</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="input.php">Tambah buku</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="#">Link</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="logout.php">Logout</a>
-                </li>
-            </ul>
+
+    <?= $components->sidebar($username) ?>
+
+
+    <div>
+        <div class="row py-3">
+            <div class="col-10 col-md-8 mx-auto">
+                <div class="col-sm">
+                    <h2>Tambah buku baru</h2>
+                </div>
+            </div>
         </div>
     </div>
 
-    <main>
-        <nav class="navbar navbar-light bg-light">
-            <div class="container">
 
-                <div class="d-flex align-items-center">
-                    <button type="button" class="navbar-toggler border-0" data-bs-toggle="offcanvas" data-bs-target="#sidebar" aria-controls="sidebar">
-                        <span class="navbar-toggler-icon"></span>
-                    </button>
-                    <a class="navbar-brand ms-1" href="#">Sibulain</a>
-                </div>
+    <div>
+        <div class="row py-2">
+            <div class="col-10 col-md-5 mx-auto pb-5">
 
-                <div class="dropdown">
-                    <button type="button" class="btn dropdown-toggle" id="profile" data-bs-toggle="dropdown" aria-expanded="false">
-                        <span class="text-muted"><?= $username ?></span>
-                    </button>
-                    <ul class="dropdown-menu" aria-labelledby="profile">
-                        <li><a class="dropdown-item" href="#">Profile</a></li>
-                        <li><a class="dropdown-item" href="logout.php">Logout</a></li>
-                    </ul>
-                </div>
+                <form action="prosesinput.php" method="post">
 
-            </div>
-        </nav>
-
-        <div>
-            <div class="row py-3">
-                <div class="col-10 col-md-8 mx-auto">
-                    <div class="col-sm">
-                        <h2>Tambah buku baru</h2>
+                    <div class="mb-3">
+                        <label for="gambar" class="form-label">Foto</label>
+                        <input class="form-control" type="file" name="txtfoto" id="gambar" required>
                     </div>
-                </div>
+
+                    <div class="mb-3">
+                        <label for="judul" class="form-label">Judul</label>
+                        <input type="text" class="form-control" id="judul" name="txtjudul" placeholder="Judul buku" required>
+                    </div>
+
+                    <div class="mb-3">
+                        <label for="deskripsi" class="form-label">Deskripsi</label>
+                        <textarea class="form-control" id="deskripsi" rows="3" name="txtdeskripsi"></textarea>
+                    </div>
+
+                    <div class="mb-3">
+                        <label for="pengarang" class="form-label">Pengarang</label>
+                        <input type="text" class="form-control" id="pengarang" name="txtpengarang" placeholder="J. K. Rowling" required>
+                    </div>
+
+                    <div class="mb-3">
+                        <label for="penerbit" class="form-label">Penerbit</label>
+                        <input type="text" class="form-control" id="penerbit" name="txtpenerbit" placeholder="Gramedia" required>
+                    </div>
+
+                    <div class="mb-3">
+                        <label for="stok" class="form-label">Stok</label>
+                        <input type="number" class="form-control" id="stok" name="txtstok" placeholder="1000" required>
+                    </div>
+
+                    <div class="mb-3">
+                        <label for="harga" class="form-label">Harga</label>
+                        <input type="number" class="form-control" id="harga" name="txtharga" placeholder="RP.54,000" required>
+                    </div>
+
+
+                    <div class="d-flex justify-content-between">
+                        <button class="btn btn-outline-secondary" type="reset">Batal</button>
+                        <button class="btn btn-primary" type="submit">Simpan</button>
+                    </div>
+
+                </form>
+
             </div>
         </div>
-
-
-        <div>
-            <div class="row py-2">
-                <div class="col-10 col-md-5 mx-auto pb-5">
-
-                    <form action="prosesinput.php" method="post">
-
-                        <div class="mb-3">
-                            <label for="gambar" class="form-label">Foto</label>
-                            <input class="form-control" type="file" name="txtfoto" id="gambar" required>
-                        </div>
-
-                        <div class="mb-3">
-                            <label for="judul" class="form-label">Judul</label>
-                            <input type="text" class="form-control" id="judul" name="txtjudul" placeholder="Judul buku" required>
-                        </div>
-
-                        <div class="mb-3">
-                            <label for="deskripsi" class="form-label">Deskripsi</label>
-                            <textarea class="form-control" id="deskripsi" rows="3" name="txtdeskripsi"></textarea>
-                        </div>
-
-                        <div class="mb-3">
-                            <label for="pengarang" class="form-label">Pengarang</label>
-                            <input type="text" class="form-control" id="pengarang" name="txtpengarang" placeholder="J. K. Rowling" required>
-                        </div>
-
-                        <div class="mb-3">
-                            <label for="penerbit" class="form-label">Penerbit</label>
-                            <input type="text" class="form-control" id="penerbit" name="txtpenerbit" placeholder="Gramedia" required>
-                        </div>
-
-                        <div class="mb-3">
-                            <label for="stok" class="form-label">Stok</label>
-                            <input type="number" class="form-control" id="stok" name="txtstok" placeholder="1000" required>
-                        </div>
-
-                        <div class="mb-3">
-                            <label for="harga" class="form-label">Harga</label>
-                            <input type="number" class="form-control" id="harga" name="txtharga" placeholder="RP.54,000" required>
-                        </div>
-
-
-                        <div class="d-flex justify-content-between">
-                            <button class="btn btn-outline-secondary" type="reset">Batal</button>
-                            <button class="btn btn-primary" type="submit">Simpan</button>
-                        </div>
-
-                    </form>
-
-                </div>
-            </div>
-        </div>
+    </div>
 
     </main>
 
