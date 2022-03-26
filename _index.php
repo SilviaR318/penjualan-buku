fd122e36e90419506b4c0cd1624fd0770385e5b7
Galang Aidil Akbar <73085251+Galangaidil@users.noreply.github.com>
2022-03-26 13:04:23 +0700

    Final Stages

--- _index.php
+++ _index.php
@@ -10,11 +10,7 @@ $components = new Components();
 <html lang="en">
 
 <head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link rel="icon" type="image/png" href="images/home.png">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
-    <title>SIBULAIN</title>
+    <?= $components->head("Welcome") ?>
 </head>
 
 <body>
@@ -36,7 +32,7 @@ $components = new Components();
     <div class="container">
         <div class="row">
             <div class="col-12">
-                <h1 class="text-center">Books</h1>
+                <h2 class="display-6 text-center fw-bold">Books</h2>
             </div>
 
             <div class="col">
@@ -59,23 +55,22 @@ $components = new Components();
                         $query = mysqli_query($conn, "SELECT * FROM buku");
                     }
                     $no = 0;
-                    while ($ambil_data = mysqli_fetch_array($query)) {
-                    ?>
+                    while ($ambil_data = mysqli_fetch_array($query)) : ?>
 
-                        <a class="text-decoration-none" href="detail.php?id=<?= $ambil_data['kd_buku'] ?>">
-                            <div class="col">
-                                <div class="card h-100">
+                        <div class="col">
+                            <div class="card h-100">
+                                <a class="text-decoration-none" href="detail.php?id=<?= $ambil_data['kd_buku'] ?>">
                                     <img src="images/<?= $ambil_data['foto'] ?>" class="card-img-top" alt="...">
                                     <div class="card-body">
-                                        <h5 class="card-title"><?= $ambil_data['pengarang'] ?></h5>
+                                        <h5 class="card-title text-black"><?= $ambil_data['pengarang'] ?></h5>
                                         <small class="text-muted">Rp<?= number_format($ambil_data['harga'], 2, ',', '.') ?></small>
                                     </div>
-                                </div>
+                                </a>
                             </div>
-                        </a>
+                        </div>
 
                     <?php
-                    }
+                    endwhile;
                     ?>
 
                 </div>
@@ -83,6 +78,10 @@ $components = new Components();
         </div>
     </div>
     <!-- Books End -->
+
+    <!-- Wave Start-->
+    <?= $components->wave() ?>
+    <!-- Wave End -->
 
     <!-- Footer Start -->
     <?= $components->footer() ?>
